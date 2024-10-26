e6fd67509c4558049c75af96ce1442c621c3d134
Huy Pham <pham.huy.19951126@gmail.com>
2024-10-26 11:04:56 +0700

    add React lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import Banner from "./components/Banner";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import About from "./components/About";
+// import Banner from "./components/Banner";
+// import Header from "./components/Header";
+// import Nav from "./components/Nav";
+// import About from "./components/About";
 
-import Contact from "./components/Contact";
+// import Contact from "./components/Contact";
 
-import PersonalProjects from "./components/PersonalProjects";
-import RealProjects from "./components/RealProjects";
+// import PersonalProjects from "./components/PersonalProjects";
+// import RealProjects from "./components/RealProjects";
+
+const Header = React.lazy(() => import("./components/Header"));
+const Nav = React.lazy(() => import("./components/Nav"));
+const About = React.lazy(() => import("./components/About"));
+const Contact = React.lazy(() => import("./components/Contact"));
+const PersonalProjects = React.lazy(() => import("./components/PersonalProjects"));
+const RealProjects = React.lazy(() => import("./components/RealProjects"));
+const Banner = React.lazy(() => import("./components/Banner"));
+
 
 function App() {
   return (
